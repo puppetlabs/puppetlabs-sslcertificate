8173397943773f0dd980b95ced46dcf0d214daae
Drew Wilson <dwilson@opentable.com>
2013-07-09 11:49:31 +0100

    Removed require from exec

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,6 @@ define sslcertificate($name, $password, $location, $root_store = 'LocalMachine',
     path      => "${ps_path};${::path}",
     command   => "${ps_command} -Command \"Import-Module WebAdministration; \$pfx = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2; \$pfxPass = ConvertTo-SecureString \\\"${password}\\\" -asplaintext -force; \$pfx.import(\\\"${cert_path}\\\",\$pfxPass,\\\"Exportable,PersistKeySet\\\"); \$store = New-Object System.Security.Cryptography.X509Certificates.X509Store(\\\"${store_dir}\\\", \\\"${root_store}\\\"); \$store.open(\\\"MaxAllowed\\\"); \$store.add(\$pfx); \$store.close();\"",
     onlyif    => "${ps_command} -Command \"Import-Module WebAdministration; if(Get-ChildItem cert:\\ -Recurse | Where-Object {\$_.FriendlyName -match \\\"${name}\\\" } | Select-Object -First 1) { exit 1 } else { exit 0 }\"",
-    require   => File[$cert_path],
     logoutput => true,
   }
 }
