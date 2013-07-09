3afff35f45140f7ffd10edcf2ad071254ed8019e
Drew Wilson <dwilson@opentable.com>
2013-07-09 09:28:07 +0100

    simplified sslcertificate define

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,18 @@
 define sslcertificate($name, $password, $location, $root_store = 'LocalMachine', $store_dir = 'My') {
-  include 'sslcertificate::param::powershell'
-
   validate_re($name, ['^(.)+$'],"Must pass name to ${module_name}\[${title}\]")
   validate_re($password, ['^(.)+$'],"Must pass password to ${module_name}\[${title}\]")
   validate_re($location, ['^(.)+$'],"Must pass location to ${module_name}\[${title}\]")
+
+  $ps_command = 'powershell.exe -ExecutionPolicy RemoteSigned'
+  $ps_path = 'C:\Windows\sysnative\WindowsPowershell\v1.0'
 
   $filename = "${name}.pfx"
   $cert_path = "${location}\\${filename}"
 
   exec { "Install-SSL-Certificate-${name}":
-    path      => "${sslcertificate::param::powershell::path};${::path}",
-    command   => "${sslcertificate::param::powershell::command} -Command \"Import-Module WebAdministration; \$pfx = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2; \$pfxPass = ConvertTo-SecureString \\\"${password}\\\" -asplaintext -force; \$pfx.import(\\\"${cert_path}\\\",\$pfxPass,\\\"Exportable,PersistKeySet\\\"); \$store = New-Object System.Security.Cryptography.X509Certificates.X509Store(\\\"${store_dir}\\\", \\\"${root_store}\\\"); \$store.open(\\\"MaxAllowed\\\"); \$store.add(\$pfx); \$store.close();\"",
-    onlyif    => "${sslcertificate::param::powershell::command} -Command \"Import-Module WebAdministration; if(Get-ChildItem cert:\\ -Recurse | Where-Object {\$_.FriendlyName -match \\\"${name}\\\" } | Select-Object -First 1) { exit 1 } else { exit 0 }\"",
+    path      => "${ps_path};${::path}",
+    command   => "${ps_command} -Command \"Import-Module WebAdministration; \$pfx = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2; \$pfxPass = ConvertTo-SecureString \\\"${password}\\\" -asplaintext -force; \$pfx.import(\\\"${cert_path}\\\",\$pfxPass,\\\"Exportable,PersistKeySet\\\"); \$store = New-Object System.Security.Cryptography.X509Certificates.X509Store(\\\"${store_dir}\\\", \\\"${root_store}\\\"); \$store.open(\\\"MaxAllowed\\\"); \$store.add(\$pfx); \$store.close();\"",
+    onlyif    => "${ps_command} -Command \"Import-Module WebAdministration; if(Get-ChildItem cert:\\ -Recurse | Where-Object {\$_.FriendlyName -match \\\"${name}\\\" } | Select-Object -First 1) { exit 1 } else { exit 0 }\"",
     require   => File[$cert_path],
     logoutput => true,
   }
--- spec/defines/sslcertificate_spec.rb
+++ spec/defines/sslcertificate_spec.rb
@@ -13,8 +13,6 @@ describe 'sslcertificate', :type => :define do
         :store_dir  => 'My',
     } }
 
-    it { should include_class('sslcertificate::param::powershell') }
-
     it { should contain_exec('Install-SSL-Certificate-testCert').with ({
       'command' => "#{powershell} -Command \"Import-Module WebAdministration; \$pfx = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2; \$pfxPass = ConvertTo-SecureString \\\"testPass\\\" -asplaintext -force; \$pfx.import(\\\"C:\\SslCertificates\\testCert.pfx\\\",\$pfxPass,\\\"Exportable,PersistKeySet\\\"); \$store = New-Object System.Security.Cryptography.X509Certificates.X509Store(\\\"My\\\", \\\"LocalMachine\\\"); \$store.open(\\\"MaxAllowed\\\"); \$store.add(\$pfx); \$store.close();\"",
       'onlyif'  => "#{powershell} -Command \"Import-Module WebAdministration; if(Get-ChildItem cert:\\ -Recurse | Where-Object {\$_.FriendlyName -match \\\"testCert\\\" } | Select-Object -First 1) { exit 1 } else { exit 0 }\"",
