8d15be576e7b6d6e738508a37fd6ae9b45c85248
Ricardo Gaspar <ricardo.filipe.chaves.gaspar@cern.ch>
2017-03-23 18:12:07 +0100

    Adding a new argument to specify the location of the scripts. README updated with this info. Spec tests added. Tests use a different certificate thumbprint.The test certificate is spec/testCert.pfx
    fixing style errors,rubocop errors
    
     Committer: ricardogaspar2 <ricardogaspar2@gmail.com>

--- spec/defines/sslcertificate_spec.rb
+++ spec/defines/sslcertificate_spec.rb
@@ -16,8 +16,8 @@ describe 'sslcertificate', type: :define do
 
     it do
       is_expected.to contain_exec('Install-testCert-SSLCert').with(
-        'command'  => 'c:\temp\import-testCert.ps1',
-        'onlyif'   => 'c:\temp\inspect-testCert.ps1',
+        'command'  => 'C:\temp\import-testCert.ps1',
+        'onlyif'   => 'C:\temp\inspect-testCert.ps1',
         'provider' => 'powershell'
       )
     end
@@ -40,6 +40,90 @@ describe 'sslcertificate', type: :define do
       )
     end
     it { is_expected.to contain_file('inspect-testCert-certificate.ps1').with_content(%r{\$installedCert in \$installedCerts}) }
+  end
+
+  describe 'when managing a ssl certificate specifying a directory for scripts' do
+    let(:title) { 'certificate-testCert' }
+    let(:params) do
+      {
+        name: 'testCert',
+        password: 'testPass',
+        location: 'C:\SslCertificates',
+        thumbprint: '07E5C1AF7F5223CB975CC29B5455642F5570798B',
+        root_store: 'LocalMachine',
+        store_dir: 'My',
+        scripts_dir: 'C:\scripts'
+      }
+    end
+
+    it do
+      is_expected.to contain_exec('Install-testCert-SSLCert').with(
+        'command'  => 'C:\scripts\import-testCert.ps1',
+        'onlyif'   => 'C:\scripts\inspect-testCert.ps1',
+        'provider' => 'powershell'
+      )
+    end
+
+    it do
+      is_expected.to contain_file('import-testCert-certificate.ps1').with(
+        'ensure'  => 'present',
+        'path'    => 'C:\\scripts\\import-testCert.ps1',
+        'require' => 'File[C:\scripts]'
+      )
+    end
+
+    it { is_expected.to contain_file('import-testCert-certificate.ps1').with_content(%r{store.Add}) }
+
+    it do
+      is_expected.to contain_file('inspect-testCert-certificate.ps1').with(
+        'ensure'  => 'present',
+        'path'    => 'C:\\scripts\\inspect-testCert.ps1',
+        'require' => 'File[C:\scripts]'
+      )
+    end
+    it { is_expected.to contain_file('inspect-testCert-certificate.ps1').with_content(%r{\$installedCert in \$installedCerts}) }
+  end
+
+  describe 'when managing a ssl certificate and set the key as not exportable' do
+    let(:title) { 'certificate-testCert' }
+    let(:params) do
+      {
+        name: 'testCert',
+        password: 'testPass',
+        location: 'C:\SslCertificates',
+        thumbprint: '07E5C1AF7F5223CB975CC29B5455642F5570798B',
+        root_store: 'LocalMachine',
+        store_dir: 'My',
+        is_exportable: false
+      }
+    end
+
+    it do
+      is_expected.to contain_exec('Install-testCert-SSLCert').with(
+        'command'  => 'C:\temp\import-testCert.ps1',
+        'onlyif'   => 'C:\temp\inspect-testCert.ps1',
+        'provider' => 'powershell'
+      )
+    end
+
+    it do
+      is_expected.to contain_file('import-testCert-certificate.ps1').with(
+        'ensure'  => 'present',
+        'path'    => 'C:\\temp\\import-testCert.ps1',
+        'require' => 'File[C:\temp]'
+      )
+    end
+
+    it { is_expected.to contain_file('import-testCert-certificate.ps1').without_content(%r{Exportable,PersistKeySet}) }
+
+    it do
+      is_expected.to contain_file('inspect-testCert-certificate.ps1').with(
+        'ensure'  => 'present',
+        'path'    => 'C:\\temp\\inspect-testCert.ps1',
+        'require' => 'File[C:\temp]'
+      )
+    end
+    it { is_expected.to contain_file('inspect-testCert-certificate.ps1').without_content(%r{Exportable,PersistKeySet}) }
   end
 
   describe 'when empty certificate name is provided' do
